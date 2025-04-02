08bb3da0c9fa5e80e991ab9dd31055126490c239
jerzyjez <tolinski.jerzy1@gmail.com>
2025-04-02 11:45:18 +0200

    koniec wersia koncowa

--- k.p.n.py
+++ k.p.n.py
@@ -2,7 +2,8 @@ import random
 
 opcje=("kamien"," papier"," nozyce")
 a=random.choice(opcje)
-b=input("podaj swoja opcje;")
+b=input("podaj swoja opcje(bez polskich znakow i z malej litery);")
+print("komputer wybral ",a)
 if a==b:
     print("remis")
 elif a=="kamien" and b=="papier":
@@ -11,3 +12,13 @@ elif a=="nozyce" and b=="papier":
     print("przegrales")
 elif a=="kamien" and b=="nozyce":
     print("przegrales")
+elif a=="papier" and b=="nozyce":
+    print("wygrales")
+elif a=="nozyce" and b=="nozyce":
+    print("wygrales")
+elif a=="papier" and b=="kamien":
+    print("przegrales")
+else:
+    print("Niepoprawna opcja wpisana")
+
+
