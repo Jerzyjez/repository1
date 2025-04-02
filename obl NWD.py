670cc20c37be1701c278df71120358141fb62392
jerzyjez <tolinski.jerzy1@gmail.com>
2025-04-02 12:01:11 +0200

    enter

--- obl NWD.py
+++ obl NWD.py
@@ -1,5 +1,5 @@
-a=int(input("podaj pierwsza liczbe calkowita"))
-b=int(input("podaj druga liczbe calokowita"))
+a=int(input("podaj pierwsza liczbe calkowita wieksza od zera"))
+b=int(input("podaj druga liczbe calokowita  wieksza od zera"))
 while a!=b:
     if a>b:
         a-=b
