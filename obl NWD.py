7f84d71248a925bce95ff80249f3a6896629c7ac
jerzyjez <tolinski.jerzy1@gmail.com>
2025-04-09 11:57:01 +0200

    complited version

--- obl NWD.py
+++ obl NWD.py
@@ -1,9 +1,16 @@
 a=int(input("podaj pierwsza liczbe calkowita wieksza od zera"))
 b=int(input("podaj druga liczbe calokowita  wieksza od zera"))
-while a!=b:
-    if a>b:
-        a-=b
-    else:
-        b-=a
-print(a)
+if a and b>0:
+        while a!=b:
+            if a>b:
+                a-=b
+            else:
+                b-=a
+        print(a)
+elif a<=0 and b<=0:
+    print("obie liczby nie sa >0")
+elif b<=0:
+    print("druga liczba <0")
+else:
+    print("druga liczba <0 ")
     
