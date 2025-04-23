c42f1f1ce581e05955c5bc8437867c31f304642a
jerzyjez <tolinski.jerzy1@gmail.com>
2025-04-23 12:10:21 +0200

    §1234567890-

--- NWW.py
+++ NWW.py
@@ -2,10 +2,17 @@ a=int(input("podaj pierwsza liczbe calkowita wieksza od zera"))
 b=int(input("podaj druga liczbe calokowita  wieksza od zera"))
 c=a
 d=b
-while a!=b:
-    if a>b:
-        a-=b
-    else:
-        b-=a
-e=c*d/a
-print(e)
+if a>0 and b>0:
+    while a!=b:
+        if a>b:
+            a-=b
+        else:
+            b-=a
+    e=c*d/a
+    print(e)
+elif a<=0 and b<=0:
+    print("obie liczby nie sa >0")
+elif b<=0:
+    print("druga liczba <0")
+else:
+    print("pierwsza liczba <0 ")
